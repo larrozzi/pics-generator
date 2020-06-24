8afb35dca621592e4d301fa24bada20a480bbc91
Yassine <larrozzi@gmail.com>
2020-06-23 23:10:43 -0400

    styling done

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../styles/imageList.css'
+import ImageCard from './imageCard'
 
 const ImageList = (props) => {
-    const images= props.images.map(({description,id,urls}) =>{
-        return <img alt= {description} key ={id} src ={urls.small}/> //key always goes into the root element that's being returned from a list
+    const images= props.images.map((image)=>{
+        return <ImageCard key ={image.id} image={image}/> //key always goes into the root element that's being returned from a list
     })
-    return <div> {images}</div>
+    return <div className="image-list"> {images}</div>
 }
 
 export default ImageList
